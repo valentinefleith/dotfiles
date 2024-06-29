8592e30d503d8ce5077105c0c8eccdd521ffd94d
valentinefleith <valentine.fleith@gmail.com>
2024-06-29 20:42:09 +0200

    new nvim config

---
+++ nvim/old-config/init.lua
@@ -0,0 +1,9 @@
+require("settings")
+require("mappings")
+require("install-plugins")
+require("color-scheme")
+require("telescope-setup")
+require("lsp")
+require("nvim_cmp")
+require'lspconfig'.pyright.setup{}
+
